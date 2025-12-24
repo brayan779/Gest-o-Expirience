315292b4104519bc64c9d1952fc16d2189761df2
brayan779 <brayan2.0yh@gmail.com>
2025-12-24 19:25:39 -0300

    Ajuste de ALLOWED_HOSTS para o servidor

--- meu_cadastro/meu_cadastro/settings.py
+++ meu_cadastro/meu_cadastro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ue@&x$=mva0*tkemw+yl1db6i5j6c4llebo2!!*b^hqtid&baz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['BrayanMath.pythonanywhere.com']
 
 
 # Application definition
